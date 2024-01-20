aacb52229f6b8f8ee88ec9fd9e325cfe9acaec60
Arseny <20mineslend14@gmail.com>
2024-01-20 15:27:55 +0500

    add config for mygitignore

--- src/main/java/com/example/mygit/utils/MyGitignoreHelper.java
+++ src/main/java/com/example/mygit/utils/MyGitignoreHelper.java
@@ -2,6 +2,7 @@ package com.example.mygit.utils;
 
 import lombok.Getter;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -16,11 +17,14 @@ import java.util.stream.Collectors;
 @Component
 @Getter
 public class MyGitignoreHelper {
+    @Value("${app.mygitignore-file}")
+    private String mygitignoreFilePath;
+
     private Collection<String> ignoreFilter;
 
     @EventListener(ApplicationStartedEvent.class)
     public void readGitIgnore() {
-        try (FileInputStream fis = new FileInputStream(".mygitignore")) {
+        try (FileInputStream fis = new FileInputStream(mygitignoreFilePath)) {
             String data = IOUtils.toString(fis, StandardCharsets.UTF_8);
             ignoreFilter = Arrays.stream(data.split("\n|\r\n"))
                     .collect(Collectors.toSet());
